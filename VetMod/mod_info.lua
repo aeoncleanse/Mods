20b99ac6178239516ac3c63f7590774cd7b8cac2
IceDreamer <ben-knight@bluebottle.com>
2015-11-04 22:13:10 +0000

    V8

--- VetMod/mod_info.lua
+++ VetMod/mod_info.lua
@@ -1,7 +1,7 @@
 name = "VetMod"
-version = 7
+version = 8
 copyright = "IceDreamer"
 description = "Redesign the veterancy system to use mass destroyed and not insta-heal units"
-uid = "1149ABC3-EF46-98FE-DEF3-35772DEF84DD"
+uid = "1149ABC3-EF47-98FE-DEF3-35772DEF84DD"
 exclusive = false
 ui_only = false
