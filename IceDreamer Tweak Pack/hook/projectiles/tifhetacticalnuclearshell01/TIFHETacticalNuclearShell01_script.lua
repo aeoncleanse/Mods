5863b4f6f3bf603153a69952c763e99563c68774
IceDreamer <ben-knight@bluebottle.com>
2015-11-04 17:51:34 +0000

    Add Initial Files

---
+++ IceDreamer Tweak Pack/hook/projectiles/tifhetacticalnuclearshell01/TIFHETacticalNuclearShell01_script.lua
@@ -0,0 +1,11 @@
+#
+# UEF Anti-Matter Shells
+#
+do
+local TArtilleryAntiMatterProjectile = import('/lua/terranprojectiles.lua').TArtilleryAntiMatterProjectile
+TIFAntiMatterShells01 = Class(TArtilleryAntiMatterProjectile) {
+	FxLandHitScale = 1.4,
+}
+
+TypeClass = TIFAntiMatterShells01
+end
