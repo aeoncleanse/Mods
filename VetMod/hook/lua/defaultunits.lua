999fd88589d949271087d34cde796a9227cd0786
IceDreamer <ben-knight@bluebottle.com>
2015-11-06 23:06:03 +0000

    Hook AirUnit into OnKilled thanks to hard overwrite :(

--- VetMod/hook/lua/defaultunits.lua
+++ VetMod/hook/lua/defaultunits.lua
@@ -19,3 +19,36 @@ ACUUnit = Class(oldACUUnit) {
         end
     end,
 }
+    
+local oldAirUnit = AirUnit
+AirUnit = Class(oldAirUnit) {
+    --- Called when the unit is killed, but before it falls out of the sky and blows up.
+    OnKilled = function(self, instigator, type, overkillRatio)
+        local bp = self:GetBlueprint()
+
+        -- A completed, flying plane expects an OnImpact event due to air crash.
+        -- An incomplete unit in the factory still reports as being in layer "Air", so needs this
+        -- stupid check.
+        if self:GetCurrentLayer() == 'Air' and self:GetFractionComplete() == 1  then
+            self.Dead = true
+            self.CreateUnitAirDestructionEffects(self, 1.0)
+            self:DestroyTopSpeedEffects()
+            self:DestroyBeamExhaust()
+            self.OverKillRatio = overkillRatio
+            self:PlayUnitSound('Killed')
+            self:DoUnitCallbacks('OnKilled')
+            self:DisableShield()
+            
+            if instigator and IsUnit(instigator) then
+                instigator:OnKilledUnit(self)
+            end
+            
+            if instigator and self.totalDamageTaken ~= 0 then
+                self:VeterancyDispersal()
+            end
+        else
+            self.DeathBounce = 1
+            MobileUnit.OnKilled(self, instigator, type, overkillRatio)
+        end
+    end,
+}
