1b693b0ea55b51560b74bbefdf9235013dc1d716
IceDreamer <ben-knight@bluebottle.com>
2015-11-15 23:23:59 +0000

    hook kirvesweapons.lua

--- BlackOpsUnleashed/lua/kirvesweapons.lua
+++ BlackOpsUnleashed/lua/kirvesweapons.lua
@@ -1,59 +1,20 @@
-local WeaponFile = import('/lua/sim/DefaultWeapons.lua')
-local KamikazeWeapon = WeaponFile.KamikazeWeapon
-local BareBonesWeapon = WeaponFile.BareBonesWeapon
-local DefaultProjectileWeapon = WeaponFile.DefaultProjectileWeapon
-local DefaultBeamWeapon = WeaponFile.DefaultBeamWeapon
-local OriginalEffectTemplate = import('/lua/EffectTemplates.lua')
-local EffectTemplate = import('/lua/kirveseffects.lua')
-
-local CollisionBeamFile = import('/lua/kirvesbeams.lua')
-
-
-
-TargetingLaser = Class(DefaultBeamWeapon) {
-    BeamType = CollisionBeamFile.TargetingCollisionBeam,
+TargetingLaserBO = Class(DefaultBeamWeapon) {
+    BeamType = CollisionBeamFile.TargetingCollisionBeamBO,
     FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_01_emit.bp'},
     
     FxBeamEndPointScale = 0.01,
 }
 
-TAAPhalanxWeapon = Class(DefaultProjectileWeapon) {
-    FxMuzzleFlash = EffectTemplate.TPhalanxGunMuzzleFlash,
-    FxShellEject  = EffectTemplate.TPhalanxGunShells,
-
-    PlayFxMuzzleSequence = function(self, muzzle)
-        DefaultProjectileWeapon.PlayFxMuzzleSequence(self, muzzle)
-        for k, v in self.FxShellEject do
-            CreateAttachedEmitter(self.unit, 'Shells_Left', self.unit:GetArmy(), v)
-            CreateAttachedEmitter(self.unit, 'Shells_Right', self.unit:GetArmy(), v)
-        end
-    end,
-}
-TAAPhalanxWeapon2 = Class(DefaultProjectileWeapon) {
-    FxMuzzleFlash = EffectTemplate.TPhalanxGunMuzzleFlash,
-    FxShellEject  = EffectTemplate.TPhalanxGunShells,
-
-    PlayFxMuzzleSequence = function(self, muzzle)
-        DefaultProjectileWeapon.PlayFxMuzzleSequence(self, muzzle)
-        for k, v in self.FxShellEject do
-            CreateAttachedEmitter(self.unit, 'Gattle_Shells_Right_01', self.unit:GetArmy(), v)
-            CreateAttachedEmitter(self.unit, 'Gattle_Shells_Right_02', self.unit:GetArmy(), v)
-            CreateAttachedEmitter(self.unit, 'Gattle_Shells_Left_01', self.unit:GetArmy(), v)
-            CreateAttachedEmitter(self.unit, 'Gattle_Shells_Left_02', self.unit:GetArmy(), v)
-        end
-    end,
-}
-
-SDFUnstablePhasonBeam = Class(DefaultBeamWeapon) {
-    BeamType = CollisionBeamFile.UnstablePhasonLaserCollisionBeam,
+SDFUnstablePhasonBeamBO = Class(DefaultBeamWeapon) {
+    BeamType = CollisionBeamFile.UnstablePhasonLaserCollisionBeam3,
     FxMuzzleFlash = {},
-    FxChargeMuzzleFlash = {}, --------OriginalEffectTemplate.SExperimentalUnstablePhasonLaserMuzzle01,
+    FxChargeMuzzleFlash = {},
     FxUpackingChargeEffects = OriginalEffectTemplate.CMicrowaveLaserCharge01,
     FxUpackingChargeEffectScale = 0.2,
 }
 
 Dummy = Class(DefaultBeamWeapon) {
-    BeamType = CollisionBeamFile.TargetingCollisionBeam,
+    BeamType = CollisionBeamFile.TargetingCollisionBeamBO,
     
     FxBeamEndPointScale = 0.01,
 }
