61082498da18403c3225ce0c918f6a028c88c496
IceDreamer <ben-knight@bluebottle.com>
2016-07-12 00:22:29 +0100

    Remove other mods from develop branch

--- IceDreamer Tweak Pack/mod_info.lua
+++
@@ -1,7 +0,0 @@
-name = "IceDreamer Tweak Pack V6"
-version = 6
-copyright = "IceDreamer and Resin_Smoker"
-description = "Serious Tweak Pack plus more changes which might not be so popular, some of which are my own."
-uid = "7749EEA3-DC14-11EA-ABC8-35772EEE74DD"
-exclusive = false
-ui_only = false
--- VetMod/mod_info.lua
+++
@@ -1,7 +0,0 @@
-name = "VetMod"
-version = 12
-copyright = "IceDreamer"
-description = "Redesign the veterancy system to use mass destroyed and not insta-heal units"
-uid = "1149ABC3-3EF2-98AE-DEF3-35772DCF84DD"
-exclusive = false
-ui_only = false
