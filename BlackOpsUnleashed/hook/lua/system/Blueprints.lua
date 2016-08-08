c82e3df431424b1174fabd8064cbf88016f7953a
Uveso <Archivar@Zwergenteam.de>
2016-08-08 19:22:08 +0200

    Adding LOD calculation to ModBlueprint loop (#144)
    * Adding LOD calculation to ModBlueprint loop
    
    * Removing DebugOptions
    
    * Exclude Special units from setting IconFadeInZoom to 130
    
    Some units like BildBoteffect and Jammer Crystals have lod settings over
    2000. So i excluded those units because they should not display a
    strategic icon to early.

--- BlackOpsUnleashed/hook/lua/system/Blueprints.lua
+++ BlackOpsUnleashed/hook/lua/system/Blueprints.lua
@@ -120,6 +120,65 @@ do
                     end
                 end
             end
+            if not bp.Categories or not bp.Display.Mesh.LODs then continue end
+            CalculateNewLod(bp)
         end
+    end
+end
+
+function CalculateNewLod(uBP)
+    -- we only check if we have SelectionSizeX + Z
+    if not uBP.SelectionSizeX or not uBP.SelectionSizeZ then return end
+    -- if we don't have LOD settings, return
+    if table.getn(uBP.Display.Mesh.LODs) <= 0 then return end
+    -- copy categories into local variable for faster access
+    local Categories = {}
+    for _, cat in uBP.Categories do
+        Categories[cat] = true
+    end
+    -- If we use only higrestextures we move LODCutoff from LOD-2 to LOD-1 and delete the 2nd LOD entry
+    -- do we have LODCutoff in 2nd LOD array ? 
+    if uBP.Display.Mesh.LODs[2].LODCutoff then
+        -- copy LODCutoff from lowres to highres LOD
+        uBP.Display.Mesh.LODs[1].LODCutoff = uBP.Display.Mesh.LODs[2].LODCutoff
+    end
+    -- delete the 2nd LOD entry
+    uBP.Display.Mesh.LODs[2] = nil
+    -- calculate unit/building surface by SelectionSize or HitBoxSize, whatever is bigger
+    local SelectionSize = uBP.SelectionSizeX * uBP.SelectionSizeZ
+    local HitBoxSize = uBP.SizeX / 1.4  * uBP.SizeZ / 1.4
+    local UnitLodSize
+    if SelectionSize > HitBoxSize then
+        UnitLodSize = SelectionSize
+    else
+        UnitLodSize = HitBoxSize
+    end
+    -- make Experimental bigger, if it's not already as big as an factory. So we see experimentals a bit longer if zoomed out
+    if (Categories.EXPERIMENTAL or uBP.StrategicIconName == 'icon_experimental_generic') and UnitLodSize < 5.5 then
+        UnitLodSize = UnitLodSize * 1.8
+    end
+    -- mq use 125 as offset, so we start hiding units after strategic icons are shown.
+    local mq = math.floor(UnitLodSize *35)+125 -- to compare: UnitLodSize from LandFactory x*y = 10,56.
+    -- dont hide units until we display the strategic icon
+    if mq < 130 then
+        mq = 130
+    end
+    -- stop displaying land units at LOD 600 (LANDFactory has ~ 500)
+    if mq > 600 and not Categories.AIR and not Categories.EXPERIMENTAL then
+        mq = 600
+    end
+    -- stop displaying air units a little bit later then land units
+    if mq > 700 and Categories.AIR and not Categories.EXPERIMENTAL then
+        mq = 700
+    end
+    -- stop displaying all units over LOD 1000. (almost zoomed out max)
+    if mq > 1000 then
+        mq = 1000
+    end
+    -- Set the new LODCutoff inside blueprint LOD-1 or LOD-2
+    uBP.Display.Mesh.LODs[1].LODCutoff = mq
+    -- display strategic icons from LOD 130 on, so they will hide smaller units. Except special units.
+    if uBP.Display.Mesh.IconFadeInZoom and uBP.Display.Mesh.IconFadeInZoom < 1000 then
+        uBP.Display.Mesh.IconFadeInZoom = 130
     end
 end
