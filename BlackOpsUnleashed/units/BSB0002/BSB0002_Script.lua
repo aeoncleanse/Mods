47cb481c5a3ba7d84879cc5068307d870f47606f
IceDreamer <ben-knight@bluebottle.com>
2016-09-10 01:37:54 +0100

    Begin to re-work the Seraphim Rift Gateway

--- BlackOpsUnleashed/units/BSB0002/BSB0002_Script.lua
+++ BlackOpsUnleashed/units/BSB0002/BSB0002_Script.lua
@@ -10,11 +10,25 @@ local SLandFactoryUnit = import('/lua/seraphimunits.lua').SLandFactoryUnit
 BSB0002 = Class(SLandFactoryUnit) {
     Parent = nil,
 
-    SetParent = function(self, parent, droneName)
-        self.Parent = parent
-        self.Drone = droneName
+    OnStartBuild = function(self, built, order)
+        SLandFactoryUnit.OnStartBuild(self, built, order)
+
+        local bp = built:GetBlueprint()
+        local massDrain = bp.Economy.BuildCostMass
+        local energyDrain = bp.Economy.BuildCostEnergy
+
+        -- Set resource drain to amounts derived from unit blueprints
+        self:SetConsumptionPerSecondMass(massDrain / 100)
+        self:SetConsumptionPerSecondEnergy(energyDrain * 3)
+    end,
+
+    OnStopBuild = function(self, built)
+        SLandFactoryUnit.OnStopBuild(self, built)
+
+        built:SetVeterancy(5)
     end,
     
+    -- Make this unit invulnerable
     OnDamage = function()
     end,
 }
--- BlackOpsUnleashed/units/BSB2402/BSB2402_Script.lua
+++ BlackOpsUnleashed/units/BSB2402/BSB2402_Script.lua
@@ -6,13 +6,12 @@
 local SLandFactoryUnit = import('/lua/seraphimunits.lua').SLandFactoryUnit
 
 BSB2402 = Class(SLandFactoryUnit) {
-    OnStopBeingBuilt = function(self,builder,layer)
-        SLandFactoryUnit.OnStopBeingBuilt(self,builder,layer)
-        local army = self:GetArmy()
-        -- Global Varibles
-        self.Side = 0
-        self.DroneTable = {}
+    OnStopBeingBuilt = function(self, builder, layer)
+        SLandFactoryUnit.OnStopBeingBuilt(self, builder, layer)
+
         self.EffectsBag = {}
+        local army = self:GetArmy()
+
         CreateAttachedEmitter(self,'Effect01',army, '/effects/emitters/seraphim_rift_arch_base_01_emit.bp'):ScaleEmitter(0.7)    -- glow
         CreateAttachedEmitter(self,'Effect01',army, '/effects/emitters/seraphim_rift_arch_base_02_emit.bp'):ScaleEmitter(0.7)    -- plasma pillar
         CreateAttachedEmitter(self,'Effect01',army, '/effects/emitters/seraphim_rift_arch_base_03_emit.bp'):ScaleEmitter(0.7)    -- darkening pillar
@@ -51,94 +50,63 @@ BSB2402 = Class(SLandFactoryUnit) {
         CreateAttachedEmitter(self,'FX_11',army, '/effects/emitters/seraphim_rift_arch_edge_01_emit.bp'):ScaleEmitter(0.7)    -- line wall
         CreateAttachedEmitter(self,'FX_12',army, '/effects/emitters/seraphim_rift_arch_edge_01_emit.bp'):ScaleEmitter(0.7)    -- line wall
         CreateAttachedEmitter(self,'FX_13',army, '/effects/emitters/seraphim_rift_arch_edge_01_emit.bp'):ScaleEmitter(0.7)    -- line wall
-        
-        -- Start the spawn threads for the Elite units
-        self:ForkThread(self.InitialSpawnFactory)
-    end,          
-    
-    InitialSpawnFactory = function(self)
-        local numcreate = 2
 
-        -- Randomly determines which launch bay will be the first to spawn a drone
-        self.Side = Random(1,2)
-        
-        -- Are we dead yet, if not spawn drones
-        if not self:IsDead() then
-            for i = 0, (numcreate -1) do
-                if not self:IsDead() then 
-                    self:ForkThread(self.SpawnFactory) 
-                    -- Short delay between spawns to spread them out
-                    WaitSeconds(1)
-                end
-            end
-        end
+        -- Set up the two additional factory rolloff points
+        self:SpawnFactory()
     end,
-    
+
+    OnStartBuild = function(self, built, order)
+        SLandFactoryUnit.OnStartBuild(self, built, order)
+
+        local bp = built:GetBlueprint()
+        local massDrain = bp.Economy.BuildCostMass
+        local energyDrain = bp.Economy.BuildCostEnergy
+
+        -- Set resource drain to amounts derived from unit blueprints
+        self:SetConsumptionPerSecondMass(massDrain / 100)
+        self:SetConsumptionPerSecondEnergy(energyDrain * 3)
+    end,
+
+    OnStopBuild = function(self, built)
+        SLandFactoryUnit.OnStopBuild(self, built)
+
+        built:SetVeterancy(5)
+    end,
+
     SpawnFactory = function(self)
-        -- Small respawn delay so the drones are not instantly respawned after death
-        -- Only respawns the drones if the parent unit is not dead
-        if not self:IsDead() then 
-
-            -- Sets up local Variables used and spawns a drone at the parents location 
+        if not self.Dead then
+            -- Get orientation of the factory gate
             local myOrientation = self:GetOrientation()
-          
-            if self.Side == 1 then
-                -- Gets the current position of the carrier launch bay in the game world
-                local location = self:GetPosition('Factory05')
 
-                -- Creates our drone in the left launch bay and directs the unit to face the same direction as its parent unit
-                local drone = CreateUnit('bsb0002', self:GetArmy(), location[1], location[2], location[3], myOrientation[1], myOrientation[2], myOrientation[3], myOrientation[4], 'Land') 
+            -- Gets the current position of the carrier launch bays in the game world
+            local leftDroneLocation = self:GetPosition('Factory05')
+            local rightDroneLocation = self:GetPosition('Factory02')
 
-                -- Adds the newly created drone to the parent carriers drone table
-                table.insert (self.DroneTable, drone)
+            -- Creates our drone in the left launch bay and directs the unit to face the same direction as its parent unit
+            self.leftDrone = CreateUnit('bsb0002', self:GetArmy(), leftDroneLocation[1], leftDroneLocation[2], leftDroneLocation[3], myOrientation[1], myOrientation[2], myOrientation[3], myOrientation[4], 'Land') 
+            self.rightDrone = CreateUnit('bsb0002', self:GetArmy(), rightDroneLocation[1], rightDroneLocation[2], rightDroneLocation[3], myOrientation[1], myOrientation[2], myOrientation[3], myOrientation[4], 'Land') 
 
-                -- Sets the Carrier unit as the drones parent
-                drone:SetParent(self, 'bsb2402')
-                drone:SetCreator(self)
+            self.leftDrone.Parent = self
+            self.rightDrone.Parent = self
 
-                -- Issues the guard command
-                IssueClearCommands({drone})
-                IssueFactoryAssist({drone}, self)
-
-                -- Flips to the next spawn point
-                self.Side = 2
-
-                --Drone clean up scripts
-                self.Trash:Add(drone)
-            elseif self.Side == 2 then
-                -- Gets the current position of the carrier launch bay in the game world
-                local location = self:GetPosition('Factory02')
-
-                -- Creates our drone in the right launch bay and directs the unit to face the same direction as its parent unit
-                local drone = CreateUnit('bsb0002', self:GetArmy(), location[1], location[2], location[3], myOrientation[1], myOrientation[2], myOrientation[3], myOrientation[4], 'Land') 
-
-                -- Adds the newly created drone to the parent carriers drone table
-                table.insert (self.DroneTable, drone)
-
-                -- Sets the Carrier unit as the drones parent
-                drone:SetParent(self, 'bsb2402')
-                drone:SetCreator(self)
-
-                -- Issues the guard command
-                IssueClearCommands({drone})
-                IssueFactoryAssist({drone}, self)
-
-                -- Flips from the right to the left self.Side after a drone has been spawned
-                self.Side = 1
-
-                --Drone clean up scripts
-                self.Trash:Add(drone)
-            end
+            -- Issues the guard command
+            IssueClearCommands({self.leftDrone})
+            IssueFactoryAssist({self.leftDrone}, self)
+            IssueClearCommands({self.rightDrone})
+            IssueFactoryAssist({self.rightDrone}, self)
         end
     end,
-    
+
     OnKilled = function(self, instigator, type, overkillRatio)
-        if table.getn({self.DroneTable}) > 0 then
-            for k, v in self.DroneTable do 
-                IssueClearCommands({self.DroneTable[k]}) 
-                IssueKillSelf({self.DroneTable[k]})
-            end 
+        -- Kill the expansion factory spawns
+        if self.leftDrone and not self.leftDrone.Dead then
+            IssueClearCommands({self.leftDrone})
+            IssueClearCommands({self.rightDrone})
+        elseif self.rightDrone and not self.rightDrone.Dead then
+            IssueKillSelf({self.leftDrone})
+            IssueKillSelf({self.rightDrone})
         end
+
         SLandFactoryUnit.OnKilled(self, instigator, type, overkillRatio)        
     end,
 }
