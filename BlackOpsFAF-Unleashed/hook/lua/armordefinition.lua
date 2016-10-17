ea11b5ecbf380860d4575621385c4abf7f882db0
IceDreamer <ben-knight@bluebottle.com>
2016-10-17 16:25:48 +0100

    Buff Flame Tank

---
+++ BlackOpsFAF-Unleashed/hook/lua/armordefinition.lua
@@ -0,0 +1,11 @@
+﻿armordefinition = {
+    {   -- Armor Type Name
+        'Structure',
+
+        -- Armor Definition
+        'Normal 1.0',
+        'Overcharge 0.066666',
+        'Deathnuke 0.032',
+        'Fire 2.5',
+    },
+}
