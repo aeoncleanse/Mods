7a65d12145db0886c30761b48f5ba8276f8feec5
IceDreamer <ben-knight@bluebottle.com>
2016-11-13 21:54:32 +0000

    hook armor properly

--- BlackOpsFAF-Unleashed/hook/lua/armordefinition.lua
+++ BlackOpsFAF-Unleashed/hook/lua/armordefinition.lua
@@ -1,11 +1,27 @@
-﻿armordefinition = {
-    {   -- Armor Type Name
-        'Structure',
+﻿newStructure = {   
+    -- Armor Type Name
+    'Structure',
 
-        -- Armor Definition
-        'Normal 1.0',
-        'Overcharge 0.066666',
-        'Deathnuke 0.032',
-        'Fire 2.5',
-    },
+    -- Armor Definition
+    'Normal 1.0',
+    'Overcharge 0.066666',
+    'Deathnuke 0.032',
+    'Fire 2.5',
 }
+
+function hookInsert(newTable)
+    for key, armorTable in armordefinition do
+        for index, str in armorTable do
+            if newTable[1] == str then
+                armordefinition[key] = newTable
+                break
+            end
+        end
+    end
+end
+
+do
+
+hookInsert(newStructure)
+
+end
