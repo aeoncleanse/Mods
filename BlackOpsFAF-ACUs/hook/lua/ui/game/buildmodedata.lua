9107be72e8792e49b1a31e10c8642081985fbe32
Uveso <Archivar@Zwergenteam.de>
2017-10-26 18:39:51 +0200

    Added file buildmodedata.lua to hook buildmodekeys for commanders

---
+++ BlackOpsFAF-ACUs/hook/lua/ui/game/buildmodedata.lua
@@ -0,0 +1,35 @@
+
+buildModeKeys = {
+--Aeon
+    -- commander
+    ['eal0001'] = {
+        [1] = AeonT1Eng,
+        [2] = AeonT2Eng,
+        [3] = AeonT3Eng,
+        [4] = AeonT4Eng,
+    },
+-- UEF
+    -- commander
+    ['eel0001'] = {
+        [1] = UEFT1Eng,
+        [2] = UEFT2Eng,
+        [3] = UEFT3Eng,
+        [4] = UEFT4Eng,
+    },
+-- Cybran
+    -- commander
+    ['erl0001'] = {
+        [1] = CybranT1Eng,
+        [2] = CybranT2Eng,
+        [3] = CybranT3Eng,
+        [4] = CybranT4Eng,
+    },
+-- Seraphim
+    -- commander
+    ['esl0001'] = {
+        [1] = SeraphimT1Eng,
+        [2] = SeraphimT2Eng,
+        [3] = SeraphimT3Eng,
+        [4] = SeraphimT4Eng,
+    },
+}
