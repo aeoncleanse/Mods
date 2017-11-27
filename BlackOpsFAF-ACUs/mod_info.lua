a1cc42af02c8bcf2f6fdc9b771474eed893fb05d
IceDreamer <bknight4125@gmail.com>
2017-11-27 19:20:10 +0000

    Bump version

--- BlackOpsFAF-ACUs/mod_info.lua
+++ BlackOpsFAF-ACUs/mod_info.lua
@@ -1,6 +1,6 @@
 name = "BlackOps FAF: ACUs"
-uid = "9e8ea941-c306-4751-b367-a00000014000"
-version = 14
+uid = "9e8ea941-c306-4751-b367-a00000016000"
+version = 16
 copyright = "2009 Exavier Macbeth, lt_hawkeye, orangeknight, IceDreamer"
 description = "The ultimate ACU upgrade pack, re-engineered for FAF 3680 or later"
 author = "Exavier Macbeth, lt_hawkeye, orangeknight - Revamped 2016 by IceDreamer"
@@ -56,4 +56,5 @@ conflicts = {
   "9e8ea941-c306-4751-b367-a00000011000",
   "9e8ea941-c306-4751-b367-a00000012000",
   "9e8ea941-c306-4751-b367-a00000013000",
+  "9e8ea941-c306-4751-b367-a00000014000"
 }
--- BlackOpsFAF-Unleashed/mod_info.lua
+++ BlackOpsFAF-Unleashed/mod_info.lua
@@ -1,6 +1,6 @@
 name = "BlackOps FAF: Unleashed"
-uid = "9e8ea941-c306-aaaf-b367-u00000014000"
-version = 14
+uid = "9e8ea941-c306-aaaf-b367-u00000016000"
+version = 16
 copyright = "2009 Exavier Macbeth, lt_hawkeye, orangeknight"
 description = "The ultimate unit pack, re-engineered for FAF 3680 or later"
 author = "Exavier Macbeth, lt_hawkeye, orangeknight - Revamped 2016 by IceDreamer"
@@ -55,4 +55,5 @@ conflicts = {
   "9e8ea941-c306-aaaf-b367-u00000011000",
   "9e8ea941-c306-aaaf-b367-u00000012000",
   "9e8ea941-c306-aaaf-b367-u00000013000",
+  "9e8ea941-c306-aaaf-b367-u00000014000",
 }
