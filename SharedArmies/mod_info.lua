453df8f9b73847bb20feb4a734b8d8a7eadcce28
IceDreamer <bknight4125@gmail.com>
2017-12-03 13:02:24 +0000

    Bump version

--- SharedArmies/mod_info.lua
+++ SharedArmies/mod_info.lua
@@ -1,6 +1,6 @@
 name = "SharedArmies"
-uid = "bea51941-c306-aaaf-b367-u00000004000"
-version = 4
+uid = "bea51941-c306-aaaf-b367-u00000005000"
+version = 5
 copyright = "2017 Eximius, Sheeo, IceDreamer, MrNukalizer"
 description = "Allows joint control of a single army"
 author = "Eximius, Sheeo, IceDreamer, MrNukalizer"
