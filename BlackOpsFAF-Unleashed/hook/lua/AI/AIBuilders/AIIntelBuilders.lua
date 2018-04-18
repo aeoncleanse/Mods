5ddbc7510ae2fec30a3f9afb9aea9b098af5186e
Uveso <Archivar@Zwergenteam.de>
2018-04-19 00:17:03 +0200

    Add builders for Hydro upgrade

--- BlackOpsFAF-Unleashed/hook/lua/AI/AIBuilders/AIIntelBuilders.lua
+++ BlackOpsFAF-Unleashed/hook/lua/AI/AIBuilders/AIIntelBuilders.lua
@@ -4,6 +4,37 @@
 BuilderGroup {
     BuilderGroupName = 'LandScoutFormBuilders',
     BuildersType = 'PlatoonFormBuilder',
+    -- Hydrocarbon Power Plant upgrade START -----------------------------------------
+    Builder {
+        BuilderName = 'T1 HydroUpgrade',
+        PlatoonTemplate = 'T1PowerHydroUpgrade',
+        Priority = 200,
+        InstanceCount = 1,
+        BuilderConditions = {
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 0, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH1 } },
+            { UCBC, 'HaveLessThanUnitsInCategoryBeingBuilt', { 1, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH2 } },
+            { EBC, 'GreaterThanEconIncome', { 2, 10 } },
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0, 0 }},
+        },
+        FormRadius = 10000,
+        BuilderType = 'Any',
+    },
+    Builder {
+        BuilderName = 'T2 HydroUpgrade',
+        PlatoonTemplate = 'T2PowerHydroUpgrade',
+        Priority = 200,
+        InstanceCount = 1,
+        BuilderConditions = {
+            { UCBC, 'HaveLessThanUnitsWithCategory', { 1, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH1 } },
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 0, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH2 } },
+            { UCBC, 'HaveLessThanUnitsInCategoryBeingBuilt', { 1, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH3 } },
+            { EBC, 'GreaterThanEconIncome', { 2.6, 60 } },
+            { EBC, 'GreaterThanEconEfficiencyOverTime', { 0, 0 }},
+        },
+        FormRadius = 10000,
+        BuilderType = 'Any',
+    },
+    -- Hydrocarbon Power Plant upgrade END -------------------------------------------
     Builder {
         BuilderName = 'T1 Land Scout Form',
         BuilderConditions = {
--- BlackOpsFAF-Unleashed/hook/lua/AI/AIBuilders/SorianIntelBuilders.lua
+++ BlackOpsFAF-Unleashed/hook/lua/AI/AIBuilders/SorianIntelBuilders.lua
@@ -3,6 +3,37 @@
 BuilderGroup {
     BuilderGroupName = 'SorianLandScoutFormBuilders',
     BuildersType = 'PlatoonFormBuilder',
+    -- Hydrocarbon Power Plant upgrade START -----------------------------------------
+    Builder {
+        BuilderName = 'Sorian T1 HydroUpgrade',
+        PlatoonTemplate = 'T1PowerHydroUpgrade',
+        Priority = 200,
+        InstanceCount = 1,
+        BuilderConditions = {
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 0, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH1 } },
+            { UCBC, 'HaveLessThanUnitsInCategoryBeingBuilt', { 1, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH2 } },
+            { SIBC, 'GreaterThanEconIncome', { 2, 10 } },
+            { SIBC, 'GreaterThanEconEfficiencyOverTime', { 0, 0 }},
+        },
+        FormRadius = 10000,
+        BuilderType = 'Any',
+    },
+    Builder {
+        BuilderName = 'Sorian T2 HydroUpgrade',
+        PlatoonTemplate = 'T2PowerHydroUpgrade',
+        Priority = 200,
+        InstanceCount = 1,
+        BuilderConditions = {
+            { UCBC, 'HaveLessThanUnitsWithCategory', { 1, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH1 } },
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 0, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH2 } },
+            { UCBC, 'HaveLessThanUnitsInCategoryBeingBuilt', { 1, categories.HYDROCARBON * categories.ENERGYPRODUCTION * categories.TECH3 } },
+            { SIBC, 'GreaterThanEconIncome', { 2.6, 60 } },
+            { SIBC, 'GreaterThanEconEfficiencyOverTime', { 0, 0 }},
+        },
+        FormRadius = 10000,
+        BuilderType = 'Any',
+    },
+    -- Hydrocarbon Power Plant upgrade END -------------------------------------------
     Builder {
         BuilderName = 'Sorian T1 Land Scout Form init',
         BuilderConditions = {
