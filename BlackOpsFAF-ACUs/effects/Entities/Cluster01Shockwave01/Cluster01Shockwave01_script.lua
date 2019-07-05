1189beb798316c1c519a31b3d119920689d2aba6
IceDreamer <bknight4125@gmail.com>
2019-07-05 03:51:46 +0100

    Turns out none of these are used and we can get rid of the lot!

--- BlackOpsFAF-ACUs/effects/Entities/Cluster01Shockwave01/Cluster01Shockwave01_script.lua
+++
@@ -1,9 +0,0 @@
-local EmitterProjectile = import('/lua/sim/defaultprojectiles.lua').EmitterProjectile
-
-Cluster01Shockwave01 = Class(EmitterProjectile) {
-    FxTrails = {'/effects/emitters/nuke_blanket_smoke_02_emit.bp'},
-    FxTrailScale = 0.03125,
-    FxTrailOffset = 0,
-}
-
-TypeClass = Cluster01Shockwave01
--- BlackOpsFAF-ACUs/effects/Entities/Cluster01Shockwave02/Cluster01Shockwave02_script.lua
+++
@@ -1,9 +0,0 @@
-local EmitterProjectile = import('/lua/sim/defaultprojectiles.lua').EmitterProjectile
-
-Cluster01Shockwave02 = Class(EmitterProjectile) {
-    FxTrails = {'/effects/emitters/nuke_blanket_smoke_01_emit.bp'},
-    FxTrailScale = 0.015625,
-    FxTrailOffset = 0,
-}
-
-TypeClass = Cluster01Shockwave02
